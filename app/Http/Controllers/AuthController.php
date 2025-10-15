ee3c96e109db3b688d98cb978bc95b8584e58ffc
barbooosa <jpmbarbosa48@gmail.com>
2025-10-14 21:01:07 -0300

    fix: auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,8 +32,17 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        $request->user()->currentAccessToken()->delete();
+        $user = $request->user();
 
-        return ['status' => true, 'message' => Geral::USUARIO_DESLOGADO];
+        if ($user) {
+            $user->currentAccessToken()->delete();
+
+            return ['status' => true, 'message' => Geral::USUARIO_DESLOGADO];
+        }
+
+        return response()->json([
+            'status' => false, 
+            'message' => 'Falha ao deslogar. Token não encontrado ou inválido.',
+        ], 401);
     }
 }
